a7eb3d1ae6ab903ec33cf7ee9ad0ef02e78518d2
Matthew Wilson <mtwilson@usc.edu>
2022-04-12 15:27:14 -0700

    dependency fix and test

--- USCRecSports/app/src/androidTest/java/edu/usc/uscrecapp/ui/SummaryVerifyRebook.java
+++ USCRecSports/app/src/androidTest/java/edu/usc/uscrecapp/ui/SummaryVerifyRebook.java
@@ -1,6 +1,5 @@
 package edu.usc.uscrecapp.ui;
 
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -14,37 +13,27 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 import android.os.AsyncTask;
-
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-
 import edu.usc.uscrecapp.R;
-import edu.usc.uscrecapp.databinding.FragmentNotificationsBinding;
-import edu.usc.uscrecapp.ui.notifications.NotificationsFragment;
 
-@LargeTest
-@RunWith(AndroidJUnit4.class)
-public class SummaryUpcomingReservationVerification {
+public class SummaryVerifyRebook {
     private static final String URL = "jdbc:mysql://10.0.2.2:3306/uscrecsports";
     private static final String USER = "root";
     private static final String PASSWORD = "Matthewwilson033!";
@@ -52,9 +41,9 @@ public class SummaryUpcomingReservationVerification {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
-
     @Test
-    public void summaryReservationVerification() {
+    public void summaryVerifyRebook() {
+        new DeleteAsync().execute();
         new ReservationAsync().execute();
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.username),
@@ -99,12 +88,25 @@ public class SummaryUpcomingReservationVerification {
                         isDisplayed()));
         bottomNavigationItemView.perform(click());
 
+        ViewInteraction button = onView(
+                allOf(withText("Cancel"),
+                        childAtPosition(
+                                allOf(withId(R.id.upcoming_layout),
+                                        childAtPosition(
+                                                withId(R.id.vertical_layout),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        button.perform(click());
+        new ReservationAsync().execute();
         ViewInteraction textView = onView(
                 allOf(withText("Lyon Center from 12:00:00 to 14:00:00 on 2024-04-09"),
                         withParent(allOf(withId(R.id.upcoming_layout),
                                 withParent(withId(R.id.vertical_layout)))),
                         isDisplayed()));
         textView.check(matches(isDisplayed()));
+
+
         new DeleteAsync().execute();
     }
 
@@ -126,21 +128,18 @@ public class SummaryUpcomingReservationVerification {
             }
         };
     }
-
     public class ReservationAsync extends AsyncTask<Void, Void, Void> {
         @Override
-        protected Void doInBackground(Void... voids) {
-            try {
+        protected Void doInBackground(Void...voids){
+            try{
                 Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement stmt = connection.createStatement();
-                String sql = "DELETE FROM reservations WHERE user_id=3";
-                stmt.executeUpdate(sql);
-
                 String sql2 = "INSERT INTO reservations(user_id,timeslot_id,location_id,date,availability_id)\n" +
                         "VALUES(3,3,1,'2024-04-09',20);";
                 stmt.executeUpdate(sql2);
 
-            } catch (Exception e) {
+            }
+            catch(Exception e){
                 Log.e("USC Rec Sports", "Error during MySQL communication", e);
 
             }
@@ -148,16 +147,17 @@ public class SummaryUpcomingReservationVerification {
         }
     }
 
-    public class DeleteAsync extends AsyncTask<Void, Void, Void> {
+    public class DeleteAsync extends AsyncTask<Void, Void, Void>{
         @Override
-        protected Void doInBackground(Void... voids) {
-            try {
+        protected Void doInBackground(Void...voids){
+            try{
                 Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement stmt = connection.createStatement();
                 String sql = "DELETE FROM reservations WHERE user_id=3";
                 stmt.executeUpdate(sql);
 
-            } catch (Exception e) {
+            }
+            catch(Exception e){
                 Log.e("USC Rec Sports", "Error during MySQL communication", e);
 
             }
@@ -165,3 +165,5 @@ public class SummaryUpcomingReservationVerification {
         }
     }
 }
+
+
